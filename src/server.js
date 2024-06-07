35c798fba7276c3e44b8f35f0b9dbb88d3f0c543
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-06-07 14:08:32 +0300

    update server

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/constants.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import { isValidObjectId } from 'mongoose';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
 
@@ -22,54 +23,20 @@ export const setupServer = () => {
 
   app.use(cors());
 
-  app.get('/', (req, res) => {
+  app.get('/', (req, res, next) => {
     res.send('Welcome');
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
+  app.use(contactsRouter);
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-  app.get('/contacts/:contactId', async (req, res) => {
-    const id = req.params.contactId;
-    if (!isValidObjectId(id)) {
-      return res.status(400).json({
-        status: 400,
-        message: `${id} is not valid`,
-      });
-    }
+  app.use('*', notFoundHandler);
 
-    const contactById = await getContactById(id);
-
-    if (!contactById) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id : ${id} was not found`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id : ${id}!`,
-      data: contactById,
-    });
-  });
-
-  app.use('*', (req, res) => {
-    res.status(404).send('Page not found');
-  });
-
-  app.use((err, req, res) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
